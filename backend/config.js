a4824952c483164ce3bbc9e6a016ee082fd643da
Adrian Calavche <adrian.calvache1@gmail.com>
2021-05-24 15:12:23 -0400

    took out aws feature for now until multers3 is resolved

--- backend/config.js
+++ backend/config.js
@@ -8,5 +8,7 @@ export default {
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || accessKeyId,
-  secretAccessKey: process.env.secretAccessKey || secretAccessKey
+  secretAccessKey: process.env.secretAccessKey || secretAccessKey.
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMIAN,
 };
--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,9 +1,10 @@
 import multer from 'multer';
 import express from 'express';
 import { isAuth } from '../utils.js';
+// multerS3 dependency is broken and doesnt want to work
+// import multerS3 from 'multer-s3;'
 import aws from 'aws-sdk';
 import { config } from 'dotenv';
-import multerS3 from 'multer-s3;'
 
 
 const uploadRouter = express.Router();
@@ -23,27 +24,27 @@ uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
   res.send(`/${req.file.path}`);
 });
 
-aws.config.update({
-  accessKeyId:config.accessKeyId,
-  secretAccessKey:config.secretAccessKey,
-});
+// aws.config.update({
+//   accessKeyId:config.accessKeyId,
+//   secretAccessKey:config.secretAccessKey,
+// });
 
-const s3 = new aws.S3();
-const storageS3 = multer ({
-  s3: s3,
-  storage: multerS3,
-  bucket: 'amazonatestbucket',
-  acl:'public_read',
-  contentType: multerS3.AUTO_CONTENT_TYPE,
-  keyBy(req, file, cb) {
-    cb(null, file.originalname)
-  },
+// const s3 = new aws.S3();
+// const storageS3 = multer ({
+//   s3: s3,
+//   storage: multerS3,
+//   bucket: 'amazonatestbucket',
+//   acl:'public_read',
+//   contentType: multerS3.AUTO_CONTENT_TYPE,
+//   keyBy(req, file, cb) {
+//     cb(null, file.originalname)
+//   },
 
-})
+// })
 
-const uploadS3 = multer ({storage:storageS3});
-router.post('/s3', uplopadS3.single('image'), (req, res) => {
-  res.send(req.file.location);
-})
+// const uploadS3 = multer ({storage:storageS3});
+// router.post('/s3', uplopadS3.single('image'), (req, res) => {
+//   res.send(req.file.location);
+// })
 
 export default uploadRouter;
